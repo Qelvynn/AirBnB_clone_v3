f442731a2cafd9c0d4c0eb7461b20d2f3e121ca8
Kelvin <qelvynn@gmail.com>
2024-05-28 22:44:31 +0000

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,23 +10,19 @@ from api.v1.views import app_views
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
-
 @app.teardown_appcontext
 def teardown(self):
     """Removes the current SQLAlchemy Session"""
-    return storage.close()
-
+    storage.close()
 
 @app.errorhandler(404)
 def error(e):
     """Handler for 404 errors"""
     return jsonify({"error": "Not found"}), 404
-
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
@@ -35,8 +31,7 @@ app.config['SWAGGER'] = {
 
 Swagger(app)
 
-
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=int(port), threaded=True)
