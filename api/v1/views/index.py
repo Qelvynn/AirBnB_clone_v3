e6adf3ea160474e2f7ce4f9db8f09eebf2ae5f99
Kelvin <qelvynn@gmail.com>
2024-05-29 21:45:46 +0000

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Index """
+"""OK - Index module.
+
+This module provides views for the API, including status and statistics.
+"""
+
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -10,16 +14,14 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of API """
+    """Status of API"""
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
-    """ Retrieves the number of each objects by type """
+    """Retrieves the number of each object by type"""
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
